fbc12e4281b7647ce76e9582f9c49886c493f290
ArunPandeyLaudari <arpa_bca2079@lict.edu.np>
2025-03-25 23:01:25 +0545

    About me in desktop size completed

--- src/assets/components/about/About.jsx
+++ src/assets/components/about/About.jsx
@@ -2,31 +2,45 @@ import React from 'react';
 
 import './About.css';
 
-import AboutImg from '../../../assets/images/about.jpg';
+import AboutImg from '../../../assets/images/arunprofile.jpg';
 
 import CV from '../../../assets/images/John-Cv.pdf';
+import Info from './Info';
+
+
 
 
 
  const About = () => {
   return (
   <section className="about section" id="about">
-
+    
     <h2 className="section__title">About Me</h2>
-
-    <span className="section__subtitle"> My Introduction</span>
-
     <div className="about__container container grid">
+
 
     <img src={AboutImg} alt="" className="about__img" />
 
     <div className="about__data">
 
-      </div>
+    <Info/>
+
+    <p className="about__description"> 
+    I am a full stack developer with a passion for creating beautiful and functional websites. I have a strong background in web development and design, and I am always looking for new ways to improve my skills and learn new technologies. I am a quick learner and a problem solver, and I am always up for a challenge. I am currently looking for new opportunities to work on exciting projects and collaborate with other talented developers. 
+
+      </p>
+
+      <a href={CV} download="" className="button button--flex">
+        Download CV 
+        
+        <i className="uil uil-download-alt button__icon"></i>
+      </a>
+
     </div>
 
+    </div>
     </section>
-  )
-}
+  );
+};
 
 export default About;
--- src/assets/components/about/Info.jsx
+++ src/assets/components/about/Info.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from 'react';
+
+import { FaAward } from "react-icons/fa";
+
+import { HiBriefcase } from "react-icons/hi";
+
+import { BiSupport } from "react-icons/bi";
+
 
 const Info = () => {
   return (
-    
-  )
-}
+    <div className="about__info grid">
+      <div className="about__box">
+        <FaAward className="about__icon" />
+        <h3 className="about__title">
+          Experience
+        </h3>
+        <span className="about__subtitle">
+          3 Years Working
+        </span>
+      </div>
+      <div className="about__box">
+        <HiBriefcase className="about__icon" />
+        <h3 className="about__title">
+          Completed
+        </h3>
+        <span className="about__subtitle">
+          30+ Projects
+        </span>
+      </div>
+      <div className="about__box">
+        <BiSupport className="about__icon" />
+        <h3 className="about__title">
+         Supports
+        </h3>
+        <span className="about__subtitle">
+          24/7
+        </span>
+      </div>
+    </div>
+  );
+};
 
 export default Info
