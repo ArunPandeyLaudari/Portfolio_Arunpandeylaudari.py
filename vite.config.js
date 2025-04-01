f4d8f3bc135aeebafa5210673b365ab7a929a1da
ArunPandeyLaudari <arpa_bca2079@lict.edu.np>
2025-04-01 09:02:05 +0545

    final commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/arunpandeylaudari2025/', // GitHub repo name
+  base: '/arunpandeylaudari2025/',
 });
