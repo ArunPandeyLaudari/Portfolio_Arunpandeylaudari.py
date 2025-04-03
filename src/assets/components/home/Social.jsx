475cafdc08d50ce7afa2a94ab9c0a328dfd081d6
ArunPandeyLaudari <arpa_bca2079@lict.edu.np>
2025-04-03 07:55:01 +0545

    final commit

--- src/assets/components/home/Social.jsx
+++ src/assets/components/home/Social.jsx
@@ -4,21 +4,21 @@ const Social = () => {
     return (
         <div className="home__social">
 
-            <a href="https://www.facebook.com" className="home__social-icon" target='_blank' rel="noopener noreferrer">
+            <a href="https://www.facebook.com/people/Arun-Pandey-Laudari/pfbid05xencrGFC6gJ2cw7HvUS3JW1Qfz7wWBnzaMZqL4E8UN2Kv4mBdjB2ejVJ4kbBFMKl/" className="home__social-icon" target='_blank' rel="noopener noreferrer">
                 <i className="uil uil-facebook-f"></i>
             </a>
 
-            <a href="https://www.linkedin.com" className="home__social-icon" target='_blank' rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/arun-pandey-laudari-214a9832a/" className="home__social-icon" target='_blank' rel="noopener noreferrer">
                 <i className="uil uil-linkedin"></i>
             </a>
 
-            <a href="https://www.instagram.com" className="home__social-icon" target='_blank' rel="noopener noreferrer">
+            <a href="https://www.instagram.com/arun_laudari1/" className="home__social-icon" target='_blank' rel="noopener noreferrer">
 
                 <i className="uil uil-instagram"></i>
             </a>
 
 
-            <a href="https://github.com" className="home__social-icon" target='_blank' rel="noopener noreferrer">
+            <a href="https://github.com/ArunPandeyLaudari" className="home__social-icon" target='_blank' rel="noopener noreferrer">
                 <i className="uil uil-github"></i>
             </a>
 
